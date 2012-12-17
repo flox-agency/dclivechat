14b5919993ab45496b62f140f11204af2521b761
jeffart <dekogha@gmail.com>
2012-12-17 14:04:35 +0100

    relation Conversation Message

--- Model/Conversation.php
+++ Model/Conversation.php
@@ -1,7 +1,16 @@
 <?php
 
 class Conversation extends AppModel {
+		
 	var $name = 'Conversation';
+	
+	var $hasMany = array(
+
+			'Message' => array(
+				'className' => 'Message',
+				'foreignKey' => 'conversation_id'
+				)
+		);
 }
 	
 	
