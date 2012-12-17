f2855e4cdde26d0064f80e7bbe42c34c37be44b0
jeffart <dekogha@gmail.com>
2012-12-17 13:36:57 +0100

    conversation

---
+++ Controller/ConversationController.php
@@ -0,0 +1,15 @@
+<?php
+
+
+
+class Conversation extends AppController {
+	public $helpers = array(’Html’, ’Form’);
+	
+	public function index() {
+       $this->set(’posts’, $this->Post->find(’all’));
+    }
+	
+}
+	
+
+?>
---
+++ Model/Conversation.php
@@ -0,0 +1,8 @@
+<?php
+
+class Conversation extends AppModel {
+	
+}
+	
+	
+?>
